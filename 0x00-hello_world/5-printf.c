5f16905af99bd6d02c33d53234f7bb9ea6099e7a
EAPwavia1 <anipass24@yahoo.com>
2023-12-15 20:41:00 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - prnts the program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("\"This is a piece of art,\n");
+	printf("\"Created with proper grammer, \n");
+	return (0);
+}
