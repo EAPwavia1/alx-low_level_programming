31d5916e5cd758c628826e16f9d041b9a6e2948b
EAPwavia1 <anipass24@yahoo.com>
2023-12-15 18:35:33 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingua puzzle);
+		return (0);
+}
