49be109aca9fb07c309f0de571957614b80ad089
EAPwavia1 <anipass24@yahoo.com>
2023-12-16 01:13:27 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("Size of a char: %1u byte(s)", sizeof(char));
+	printf("Size of a char: %1u byte(s)," sizeof(char));
 	printf("Size of a int: %1u byte(s)," sizeof(int));
 	printf("Size of a long: int %1u byte(s)," sizeof(int long));
 	printf("Size of a long: long int %1u byte(s)," sizeof(long long int));
