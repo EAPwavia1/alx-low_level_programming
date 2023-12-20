9ff1e5ea2e5c39da773085679ad0d2ca7e9506f9
EAPwavia1 <anipass24@yahoo.com>
2023-12-20 10:46:57 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "main.h"
 /**
  * print_alphabet_x10 - pintst 10 times the alphabet in lowercase
  *
@@ -6,11 +6,13 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char j;
+
 
 	for (i = 0; i < 10; i++)
 	{
-		for (i = 'a'; i <= 'z'; i++)
-			putchar(i);
-		putchar('\n');
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
 	}
+		_putchar('\n');
 }
