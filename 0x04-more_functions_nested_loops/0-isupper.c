b1ff144eaf854c26604a15ea72e7824699ec5236
EAPwavia1 <anipass24@yahoo.com>
2023-12-21 20:33:21 +0000

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,10 +7,8 @@
  */
 int _isupper(int c)
 {
-
-	c = 'A';
-		printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-		printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
