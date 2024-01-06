3d5331b995bae66546219c738f7985d82e6c9c72
EAPwavia1 <anipass24@yahoo.com>
2024-01-06 03:56:31 +0000

    101-print_numbers.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: an integer parameter
+ * Return:0
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	if (ni / 10 != 0)
+	{
+		print_nunmber(n1 / 10);
+	}
+	_putchar(n1 % 10; + 0);
+}
