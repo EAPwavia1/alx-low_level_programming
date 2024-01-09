44685d17fd6c39704d16b6eed08300410093b138
EAPwavia1 <anipass24@yahoo.com>
2024-01-09 18:51:22 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - the function locates character in a string
+ * @s: input 1
+ * @c: input 1
+ * Return: c or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
